97233c66d6296c21f6fef6ffaea3147c3c8886f2
pgongd <oik92920@gmail.com>
2021-04-20 12:10:33 +0900

    java for문을 이용한 구구단 예제

---
+++ java/기초/GuGuDan.java
@@ -0,0 +1,17 @@
+package ex0923;
+
+public class GuGuDan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=2; i<=9;i++) {
+			for(int j=1;j<=9;j++) {
+				
+				System.out.print(i+"X"+j+"="+i*j+"\t");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
